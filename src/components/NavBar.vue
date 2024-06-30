6c8ea42488d0e9d428aaf30b98168a9fd31c6ea0
joren.dhaen <joren.dhaen@student.odisee.be>
2024-06-30 17:06:13 +0200

    install vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header><span>Joren</span></header>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
