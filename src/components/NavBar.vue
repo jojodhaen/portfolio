3d374b2b11c35966abe8b71774750149e1fb13a8
joren.dhaen <joren.dhaen@student.odisee.be>
2024-07-03 00:39:24 +0200

    start setting up main components

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,48 @@
 <script lang="ts" setup></script>
 
 <template>
-  <header><span>Joren</span></header>
+  <header>
+    <div class="nav-container">
+      <span class="name">JOREN</span>
+      <nav>
+        <router-link class="nav-link" to="/">PROJECTEN</router-link>
+        <router-link class="nav-link" to="/">OVER MIJ</router-link>
+        <router-link class="nav-link" to="/">CONTACT</router-link>
+      </nav>
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  background-color: #333;
+  padding: 1rem;
+  text-align: center;
+}
+
+.nav-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+
+.name {
+  font-size: 2.5rem;
+}
+
+nav {
+  display: flex;
+  gap: 1rem;
+}
+
+.nav-link {
+  display: flex;
+  gap: 1rem;
+  text-decoration: none;
+  color: white;
+  font-size: 1.25rem;
+}
+</style>
